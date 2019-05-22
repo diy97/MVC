3aabe005d388a41134ca1bd64cfa943699a241aa
Misyo <cxj9786@qq.com>
2019-05-22 22:22:56 +0800

    FengYe
    FengYe

---
+++ AjaxProject/AjaxProject/Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using AjaxProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace AjaxProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult List(int pagesize=4,int pageindex=1)//列表页（视图）
+        {
+            //控制器-动作方法（不管业务逻辑）
+            //通过传入参数，调用业务逻辑层方法后去分页信息
+            //session,viewdata,viewbag,application,tempdata
+            ViewBag.ProList=  GetPager(pagesize, pageindex);
+            return View();
+        }
+
+        #region 业务逻辑层
+
+        /// <summary>
+        /// 产品业务逻辑层
+        /// </summary>
+        /// <param name="pagesize"></param>
+        /// <param name="pageindex"></param>
+        /// <returns></returns>
+        private Pager<Products> GetPager(int pagesize, int pageindex)
+        {
+            //查询数据库
+            //1。查询一共多少条数据
+            //2。显示数据
+            List<Products> list = GetProductsList(pagesize, pageindex);
+            Pager<Products> pager = new Pager<Products>(pageindex,pagesize,GetCount(),list);
+            return pager;
+
+        }
+
+        #endregion
+
+
+        #region 数据访问层
+
+        private int GetCount()
+        {
+            string sql = "select count(1) from Products";
+            object obj = DBHelper.ExecuteScalar(sql);
+            return (int)obj;
+        }
+        /// <summary>
+        /// 根据页码大小查询并返回
+        /// </summary>
+        /// <param name="pagesize"></param>
+        /// <param name="pageindex"></param>
+        /// <returns></returns>
+        private List<Products> GetProductsList(int pagesize, int pageindex)
+        {
+            List<Products> list = new List<Products>();
+            string sql = @"select*from(select ROW_NUMBER()over(order by productid asc) as Id,
+                         *from Products) Products where id between @start and @end";
+
+            int start, end;
+            start = (pageindex - 1) * pagesize + 1;
+            end = pageindex * pagesize;
+
+            SqlParameter[] paras = {
+                new SqlParameter("@start",start),
+                new SqlParameter("@end",end)
+            };
+            using (SqlDataReader reader = DBHelper.ExecuteReader(sql, paras))
+            {
+                while (reader.Read())
+                {
+                    Products pro = new Products();
+                    pro.ProductId = (int)reader["productId"];
+                    pro.TypeId = (int)reader["typeId"];
+                    pro.ProductName = reader["productName"].ToString();
+                    pro.Price =Convert.ToSingle( reader["price"]);
+                    pro.Descripiton = reader["Descripiton"].ToString();
+                    list.Add(pro);
+                }
+
+            }
+            return list;
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ AjaxProject/AjaxProject/Models/DBHelper.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace AjaxProject
+{
+    /// <summary>
+    /// 数据库帮助类
+    /// </summary>
+    public class DBHelper
+    {
+        private static string Connstr = "server=.;uid=sa;pwd=sa;database=SN_DB";
+        /// <summary>
+        /// 执行添加、删除、修改的方法
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] paras)
+        {
+            using (SqlConnection conn = new SqlConnection(Connstr))
+            {
+                //打开数据库连接
+                conn.Open();
+                SqlCommand command = new SqlCommand(sql, conn);
+                command.Parameters.AddRange(paras);
+                int result = command.ExecuteNonQuery();
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL并返回第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string sql,params SqlParameter[] paras)
+        {
+            using (SqlConnection conn = new SqlConnection(Connstr))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(sql, conn);
+                object obj = command.ExecuteScalar();
+                return obj;
+            }
+        }
+
+        /// <summary>
+        /// 热行SQL返回SqlDataReader对象
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="paras"></param>
+        /// <returns></returns>
+        public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] paras)
+        {
+            SqlConnection conn = new SqlConnection(Connstr);
+            conn.Open();
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.Parameters.AddRange(paras);
+            return command.ExecuteReader(CommandBehavior.CloseConnection);//命令行为
+        }
+
+        public static DataRow GetDataRow(string sql, params SqlParameter[] paras)
+        {
+            DataTable dt = null;
+            using (SqlConnection conn = new SqlConnection(Connstr))
+            {
+                SqlCommand command = new SqlCommand(sql, conn);
+                command.Parameters.AddRange(paras);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                dt = new DataTable();
+                adapter.Fill(dt);
+            }
+            if (dt.Rows.Count > 0)
+                return dt.Rows[0];
+            else
+                return null;
+        }
+
+        public static DataTable GetDataTable(string sql ,params SqlParameter[] paras)
+        {
+            DataTable dt = null;
+            using (SqlConnection conn = new SqlConnection(Connstr))
+            {
+                SqlCommand command = new SqlCommand(sql, conn);
+                command.Parameters.AddRange(paras);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                dt = new DataTable();
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        public static DataSet GetDataSet(string sql, params SqlParameter[] paras)
+        {
+            DataSet ds = null;
+            using (SqlConnection conn = new SqlConnection(Connstr))
+            {
+                SqlCommand command = new SqlCommand(sql, conn);
+                command.Parameters.AddRange(paras);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                ds = new DataSet();
+                adapter.Fill(ds);
+            }
+            return ds;
+
+
+        }
+
+    }
+}
