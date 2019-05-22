23fd2828b38248439ac05f2de62b6c202e23de48
diy97 <44125982+diy97@users.noreply.github.com>
2019-05-22 23:33:16 +0800

    Add files via upload

---
+++ Models/Pager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxProject.Models
+{
+    /// <summary>
+    /// 通用分页类，T点位符
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Pager<T>
+    {
+        //类的成员 有三种：字段、属性、方法
+
+        //当前页码：PageIndex
+        // 当前显示条数：PageSize
+        //总条数：DataCount
+        //总页数：PageCount(总条数和每页显示条数计算出来)
+        //存放数据集合：List<Products>
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int DataCount { get; set; }
+        public int PageCount { get; set; }
+
+        public List<T> InfoList { get; set; }
+
+        //无参构造方法
+        public Pager() { }
+        //有参构造方法
+        public Pager(int pageIndex, int pageSize, int dataCount, List<T> list)
+        {
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+            this.DataCount = dataCount;
+            this.PageCount = dataCount / pageSize;
+            if (dataCount % pageSize != 0)
+            {
+                this.PageCount++;
+            }
+            this.InfoList = list;
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxProject.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public int TypeId { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public string Descripiton { get; set; }
+
+    }
+}
